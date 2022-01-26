c6f3cf2a122d90e73d6e67da14bc3ffd825dff4d
l-d-b <laszlo.dora.beata@gmail.com>
2022-01-26 12:14:05 +0100

    refactor maven project

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -1,4 +1,4 @@
-package com.helloworld;
+package main;
 
 public class HelloWorld {
         public void welcome(String name) {
--- src/main/HelloWorldApp.java
+++ src/main/HelloWorldApp.java
@@ -1,8 +1,8 @@
-package com.helloworld;
+package main;
 
 public class HelloWorldApp {
     public static void main(String[] args) {
         HelloWorld helloWorld = new HelloWorld();
-        helloWorld.welcome("janice");
+        helloWorld.welcome("Janice");
     }
 }
