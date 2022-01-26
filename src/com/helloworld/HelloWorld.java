486bedc3ec0f666b68e011f085136f394a07892e
l-d-b <laszlo.dora.beata@gmail.com>
2022-01-26 10:17:23 +0100

    add HelloWorld

---
+++ src/com/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.helloworld;
+
+public class HelloWorld {
+        public void welcome(String name) {
+            if(name == null) {
+                System.out.println("Hello World!");
+            } else {
+                System.out.println("Hello " + name + "!");
+            }
+        }
+}
---
+++ src/com/helloworld/HelloWorldApp.java
@@ -0,0 +1,8 @@
+package com.helloworld;
+
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.welcome("janice");
+    }
+}
